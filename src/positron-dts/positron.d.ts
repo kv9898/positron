d59e0d0eed92cb9bf43b9ab16fde807ab842f9b8
Lionel Henry <lionel.hry@proton.me>
2025-03-28 12:20:04 +0100

    Add event for plot render settings

--- src/positron-dts/positron.d.ts
+++ src/positron-dts/positron.d.ts
@@ -1322,6 +1322,29 @@ declare module 'positron' {
 		installDependencies?: () => Promise<boolean>;
 	}
 
+	/**
+	 * Settings necessary to render a plot in the format expected by the plot widget.
+	 */
+	export interface PlotsRenderSettings {
+		size: {
+			width: number;
+			height: number;
+		};
+		pixel_ratio: number;
+		format: RenderFormat;
+	}
+
+	/**
+	 * Possible formats for rendering a plot.
+	 */
+	export enum RenderFormat {
+		Png = 'png',
+		Jpeg = 'jpeg',
+		Svg = 'svg',
+		Pdf = 'pdf',
+		Tiff = 'tiff'
+	}
+
 	namespace languages {
 		/**
 		 * Register a statement range provider.
@@ -1443,6 +1466,18 @@ declare module 'positron' {
 		 * Returns the current width of the console input, in characters.
 		 */
 		export function getConsoleWidth(): Thenable<number>;
+
+		/**
+		 * Fires when the settings necessary to render a plot in the format expected by
+		 * plot widget have changed.
+		 */
+		export const onDidChangePlotsRenderSettings: vscode.Event<PlotsRenderSettings>;
+
+		/**
+		 * Returns the settings necessary to render a plot in the format expected by
+		 * plot widget.
+		 */
+		export function getPlotsRenderSettings(): Thenable<PlotsRenderSettings>;
 	}
 
 	namespace runtime {
